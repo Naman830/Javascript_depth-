c249222c5ee25f49ae62e572ccbdfe7dc3b597b9
Naman <namansingla830@gmail.com>
2026-06-16 16:12:05 +0530

    add cast

---
+++ 02_scope&closure/02_execution_engine.js
@@ -0,0 +1,10 @@
+// The Cast
+var a = 2;
+
+/*
+1. Engine: reponsible to start and finish compilationa nd execution in javscript
+2. Compiler: handle all work parsing and code generation
+3. Scope: mantain declared variable and add a strict rule to them for current exectuing code 
+
+two distinct actions are taken for a variable assignment: First, Compiler declares a variable (if not previously declared in the current scope), and second, when executing, Engine looks up the variable in Scope and assigns to it, if found.
+*/
