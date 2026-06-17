6f06f79d435a439b9ca01cc7c735a1ad035d249a
Naman <namansingla830@gmail.com>
2026-06-17 12:04:29 +0530

    add should use with or eval in code

--- 02_scope&closure/05_with.js
+++ 02_scope&closure/05_with.js
@@ -23,3 +23,10 @@ console.log(o2.a); // undefined
 console.log(a); // 2 -- Oops, leaked global!
 
 // In addition to being a bad idea to use, both eval(..) and with are affected (restricted) by Strict Mode. with is outright disallowed, whereas various forms of indirect or unsafe eval(..) are disallowed while retaining the core functionality.
+
+// =========================================================
+// =========================================================
+// =========================================================
+// SHOULD WE USE EVAL OR WITH IN OUR CODE ANSWER IS--- NO here's why
+
+// Your code will almost certainly tend to run slower simply by the fact that you include an eval(..) or with anywhere in the code. No matter how smart the Engine may be about trying to limit the side-effects of these pessimistic assumptions, there's no getting around the fact that without the optimizations, code runs slower.
