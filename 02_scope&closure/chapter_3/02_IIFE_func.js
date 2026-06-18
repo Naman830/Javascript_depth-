b84c0768a855bcbff66e8f3c046ff54db44b432c
Naman <namansingla830@gmail.com>
2026-06-18 22:41:13 +0530

    little changes

--- 02_scope&closure/chapter_3/02_IIFE_func.js
+++ 02_scope&closure/chapter_3/02_IIFE_func.js
@@ -3,7 +3,6 @@
 // Sometimes you want a scope bubble, but don't want to pollute the enclosing scope with a function name. The IIFE pattern solves this:
 
 var a = 2;
-
 // Named function pollutes outer scope:
 function foo() {
   var a = 3;
@@ -17,5 +16,4 @@ console.log(a); // 2 — ok, but 'foo' still exists in outer scope
   var a = 3;
   console.log(a); // 3
 })(); // ← immediately invoked
-
 console.log(a); // 2 — outer 'a' untouched, 'IIFE' name doesn't exist here
