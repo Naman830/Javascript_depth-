e63dcf94e43fc4b4eef4253b69c8b0697b9537a2
Naman <namansingla830@gmail.com>
2026-06-30 10:40:04 +0530

    remove this file

--- 03_This&object_prototype/chapter_1/01_what_is_this.js
+++ 03_This&object_prototype/chapter_1/01_what_is_this.js
@@ -23,3 +23,19 @@ console.log(identify(me)); // "KYLE"
 
 // Same one function, works for any object!
 // The point: this lets one function work across many objects without hard-coding any object name inside it. It's a reusability mechanism.
+
+// ===============================================================
+// ===============================================================
+// ===============================================================
+
+// this in global is work as a global object and in different urntime enviroment you see different global object in js it shows window
+console.log(this);
+
+// this keyword works different in strict and non-strict mode and value depends on it
+// inside function
+// this object in function is different
+function x() {
+  //"use strict";
+  console.log(this);
+}
+x();
