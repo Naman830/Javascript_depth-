d1c1a1881770f793d872dd23a5fc809252204fb1
Naman <namansingla830@gmail.com>
2026-06-30 10:04:46 +0530

    this meaning in runtime

---
+++ 03_This&object_prototype/chapter_1/04_runtime_biding.js
@@ -0,0 +1,5 @@
+// this is not an author-time binding (set when you write the code). It is a runtime binding — determined entirely by how and where the function is called, not where it was defined.
+
+// IN SHORT
+// WRONG mental model: this is fixed when the function is defined
+// CORRECT mental model: this is determined at the moment of calling
